34be466b802415dabb5137a5b473afadd02eb03a
Ronak J Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2025-03-29 21:59:06 +0530

    Uodates

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ var type = url.searchParams.get("type");
 var value = url.searchParams.get("value");
 console.log(type);
 
-switch( type ){
-    case "mailto":
-        window.location.replace(`${type}:${value}`);
-    case "tel":
-        window.location.replace(`${type}:${value}`);
+if( type && url ) {
+    window.location.replace(`${type}:${value}`);
 }
